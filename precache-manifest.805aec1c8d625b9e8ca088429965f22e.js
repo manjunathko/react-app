61a9178832a1c6da3ce65d81bab297f19b12d68d
manjunathko <manjunathkankatteko@gmail.com>
2019-05-29 17:35:58 +0530

    Updates

---
+++ precache-manifest.805aec1c8d625b9e8ca088429965f22e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97f615a98ddb38546c537700997654a7",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "57673d0e1ee7a37fb707",
+    "url": "/react-app/static/css/main.8d5c6fd6.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/react-app/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "57673d0e1ee7a37fb707",
+    "url": "/react-app/static/js/main.29586b87.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "e1682e423a91134958806d6364a52442",
+    "url": "/react-app/static/media/me.e1682e42.jpg"
+  }
+]);
