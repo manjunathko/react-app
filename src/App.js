98c9c145a68533de9fcccca4b6369ab5440ec43b
manjunathko <manjunathkankatteko@gmail.com>
2019-05-29 16:39:14 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
-import React from 'react';
-import logo from './logo.svg';
+import  React from 'react';
 import './App.css';
 
-function App() {
+function timer(){
+	
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="App App-logo" style={{backgroundColor:'#cccffc',padding:'10px', height:'100%',width:'100%'}}>
+      
+	  
+	  <div style={{backgroundColor:'blue', width:'100%', height:'88px',color:'white',fontSize:'20px'}}>
+	  <p>&nbsp;</p>
+	  <p>Employee Performance Reoprt</p>
+	  </div>
+	  
+	<div style={{paddingTop:'10px'}}>
+	<img className="imgrad" src={require("./me.jpg")} alt={{}} />
+        <p>Welcome KO !!</p>
+      </div>
+	  
+	  <table className="tabledesign">
+	  <tr><th>Project name</th>
+	  <th><code>Percentage of completion</code></th>
+	  <th><code>Performance quality</code></th>
+	  </tr>
+	  
+	  <tr className="oddrow">
+	  <td>Webapplication</td>
+	  <td>80%</td>
+	  <td>Excelent</td>
+	  </tr>
+	 
+	 
+	 <tr>
+	  <td>Android ROM</td>
+	  <td>50%</td>
+	  <td>Good</td>
+	  </tr>
+	 
+	 <tr className="oddrow">
+	  <td>Server Scripting</td>
+	  <td>90%</td>
+	  <td>Excelent</td>
+	  </tr>
+	 
+	 <tr >
+	  <td>ERP</td>
+	  <td>50%</td>
+	  <td>Good</td>
+	  </tr>
+	 
+	 
+	 
+	 <tr className="oddrow">
+	  <td>Mobile app</td>
+	  <td>90%</td>
+	  <td>Excelent</td>
+	  </tr>
+	 
+	 
+	 
+	 <tr >
+	  <td>Linux kernel</td>
+	  <td>90%</td>
+	  <td>Excelent</td>
+	  </tr>
+	 
+	  </table>
+	  
+	</div>
+	
+	
   );
 }
 
-export default App;
+
+
+
+
+export default timer;
